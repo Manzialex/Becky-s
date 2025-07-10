d7cbe7db07b3b93a0d5786531b5c8dfbd3b7722e
Manzi.alex <alexbagabe@gmail.com>
2025-07-10 17:38:07 +0300

    products page changes

--- images.js
+++ images.js
@@ -1,4 +1,46 @@
-  let cart = [];
+
+let index = 0;
+const carousel = document.getElementById("productCarousel");
+const slideWidth =  300;// carousel.children[0].offsetWidth;
+const maxScrolls = 4;
+//const totalSlides = productCarousel.children.length;
+
+ function updateSlide() {
+    productCarousel.style.transform = `translateX(-${index * slideWidth}px)`;
+  }
+
+  function nextSlide() {
+    if (index < maxScrolls) { //index < totalslides -1 " removed this since next button had no end"
+      index++;
+      updateSlide();
+    };
+    
+    
+  }
+  function prevSlide() {
+    if (index > 0) {
+      index--;
+      updateSlide();
+    }
+  }
+
+
+
+
+const dragging = (e) =>{
+        // console.log(e.pageX);
+           carousel.scrollLeft = e.pageX;
+          
+}
+carousel.addEventListener("mousemove",dragging); 
+
+
+
+
+
+
+
+ let cart = [];
        let total = 0;
 
        function addToCart(name, price){
